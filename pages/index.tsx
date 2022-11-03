e5ec4501b6cb9e51a706adf4fe491b52bcf40832
sochetra NOV <sochetra12.nov@gmail.com>
2022-11-03 09:11:47 +0700

    chore: adding hybrid amp page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useAmp } from "next/amp";
 import styles from "../styles/Home.module.css";
 import AmpCard from "../src/components/AmpCard";
+
+export const config = {
+  amp: "hybrid",
+};
 
 export default function Home() {
   return (
